0e5a7cb2d97fbb0dc2edf08a4bbc8e2f90cde04a
Subathra p <subathra.tech@gmail.com>
2026-09-24 22:46:11 +0530

    Add decreasing adjacent pairs algorithm

---
+++ dsa/121_count_decreasing_adjacent_pairs.py
@@ -0,0 +1,33 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+def count_decreasing_adjacent_pairs(head):
+    if head is None or head.next is None:
+        return 0
+
+    count = 0
+    current = head
+
+    while current.next is not None:
+        if current.next.data < current.data:
+            count += 1
+
+        current = current.next
+
+    return count
+
+
+# Create linked list
+head = Node(20)
+head.next = Node(15)
+head.next.next = Node(18)
+head.next.next.next = Node(10)
+head.next.next.next.next = Node(25)
+head.next.next.next.next.next = Node(5)
+
+result = count_decreasing_adjacent_pairs(head)
+
+print("Decreasing adjacent pairs:", result)
