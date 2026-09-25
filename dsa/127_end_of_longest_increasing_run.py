ac9847ac9cecf551f790c36993742cad54dc6de2
Subathra p <subathra.tech@gmail.com>
2026-09-25 17:31:46 +0530

    Add end of longest increasing run algorithm

---
+++ dsa/127_end_of_longest_increasing_run.py
@@ -0,0 +1,44 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+def end_of_longest_increasing_run(head):
+    if head is None:
+        return None
+
+    current = head
+    current_length = 1
+
+    longest = 1
+    longest_end = head.data
+
+    while current.next is not None:
+        if current.next.data > current.data:
+            current_length += 1
+        else:
+            current_length = 1
+
+        if current_length > longest:
+            longest = current_length
+            longest_end = current.next.data
+
+        current = current.next
+
+    return longest_end
+
+
+# Create linked list
+head = Node(10)
+head.next = Node(12)
+head.next.next = Node(15)
+head.next.next.next = Node(8)
+head.next.next.next.next = Node(10)
+head.next.next.next.next.next = Node(11)
+head.next.next.next.next.next.next = Node(13)
+head.next.next.next.next.next.next.next = Node(5)
+
+result = end_of_longest_increasing_run(head)
+
+print("Ending value:", result)
