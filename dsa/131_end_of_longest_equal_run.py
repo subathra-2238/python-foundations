54cc1a7908a55552496b1f5c1a9a8fd7ed663676
Subathra p <subathra.tech@gmail.com>
2026-09-25 17:48:35 +0530

    Add end of longest equal run algorithm

---
+++ dsa/131_end_of_longest_equal_run.py
@@ -0,0 +1,46 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+def end_of_longest_equal_run(head):
+    if head is None:
+        return None
+
+    current = head
+    current_length = 1
+
+    longest = 1
+    longest_end = head.data
+
+    while current.next is not None:
+        if current.next.data == current.data:
+            current_length += 1
+        else:
+            current_length = 1
+
+        if current_length > longest:
+            longest = current_length
+            longest_end = current.next.data
+
+        current = current.next
+
+    return longest_end
+
+
+# Create linked list
+head = Node(5)
+head.next = Node(5)
+head.next.next = Node(8)
+head.next.next.next = Node(8)
+head.next.next.next.next = Node(8)
+head.next.next.next.next.next = Node(3)
+head.next.next.next.next.next.next = Node(3)
+head.next.next.next.next.next.next.next = Node(3)
+head.next.next.next.next.next.next.next.next = Node(3)
+head.next.next.next.next.next.next.next.next.next = Node(10)
+
+result = end_of_longest_equal_run(head)
+
+print("Ending value:", result)
